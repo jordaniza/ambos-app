58e96a534335d4d30555c491137ea83132ad515c
jordan <jordaniza@proton.me>
2023-09-02 19:24:34 +0400

    poly

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,24 @@ import { purgeCss } from "vite-plugin-tailwind-purgecss";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     purgeCss(),
+     nodePolyfills({
+      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
   ],
   optimizeDeps: {
     esbuildOptions: {
