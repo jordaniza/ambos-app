53886854661dc0ce7f91493bbcf12184018280e0
jordan <jordaniza@proton.me>
2023-09-19 12:29:27 +0400

    buttons and fetching theme

--- src/lib/components/ui/theme-toggle/theme-toggle.svelte
+++ src/lib/components/ui/theme-toggle/theme-toggle.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { Moon, Sun } from 'lucide-svelte';
 	import Button from '../button/button.svelte';
+	import { onMount } from 'svelte';
 
+	let theme: 'light' | 'dark' | null = null;
+
+	/**
+	 * The theme is set in the root app.html with theme colors defined in app.postcss
+	 * Load the theme from local storage in the root +layout.svelte file, then you can change the theme using this button
+	 */
 	function toggleTheme() {
 		if (!document) {
 			console.error('trying to toggle theme but document is not defined');
@@ -8,23 +16,31 @@
 		}
 		const root = document.documentElement;
 		if (root.classList.contains('dark')) {
+			theme = 'light';
 			root.classList.remove('dark');
 			localStorage.setItem('theme', 'light');
 		} else {
+			theme = 'dark';
 			root.classList.add('dark');
 			localStorage.setItem('theme', 'dark');
 		}
 	}
+
+	onMount(() => {
+		const fetchedTheme = localStorage.getItem('theme');
+		if (['dark', 'light', null].includes(fetchedTheme)) {
+			theme = fetchedTheme as 'dark' | 'light' | null;
+		} else {
+			console.warn(`uncrecognized theme ${fetchedTheme}, setting to light`);
+			theme = 'light';
+		}
+	});
 </script>
 
 <Button size="icon" on:click={toggleTheme}>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		class="h-6 w-6"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
-	>
-		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-	</svg>
+	{#if theme === 'dark'}
+		<Sun />
+	{:else}
+		<Moon />
+	{/if}
 </Button>
