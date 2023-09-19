a3d73774011695edc6bd7fe83b3c9b1110a640b9
jordan <jordaniza@proton.me>
2023-09-19 16:40:35 +0400

    update lp

--- src/routes/(landing)/components/Footer.svelte
+++ src/routes/(landing)/components/Footer.svelte
@@ -1,7 +1,7 @@
 <!--Footer-->
 <footer class="p-4">
 	<p class="text-xs italic text-center">
-		Ambos Finance is a subsidiary of PREDIXIA-FZE
+		Ambos Finance is a division of PREDIXIA-FZE
 		<br />DSO-DDP-A5-D-FLEX-1061 - Dubai Digital Park - Office A5 - Dtec - Dubai Silicon Oasis
 	</p>
 </footer>
--- src/routes/(landing)/components/Nav.svelte
+++ src/routes/(landing)/components/Nav.svelte
@@ -18,7 +18,7 @@
   `}
 >
 	<div class="px-5 w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2 lg:py-5">
-		<div class="flex items-center justify-center sm:justify-between">
+		<div class="flex w-full sm:w-auto items-center justify-center sm:justify-between">
 			<!-- svelte-ignore a11y-invalid-attribute -->
 			<a
 				class:text-gray-800={navFloat}
@@ -26,9 +26,8 @@
 				class="no-underline hover:no-underline font-bold text-lg lg:text-2xl"
 				href="#"
 			>
-				<div class="flex gap-4 items-center">
-					<img src="semicircle.png" alt="Ambos Finance Logo" height="50" width="60" class="pb-3" />
-					<h1>Ambos Finance</h1>
+				<div class="flex sm:w-auto items-center">
+					<img src="ambos-header.png" alt="Ambos Finance Logo" width="200" />
 				</div>
 			</a>
 		</div>
