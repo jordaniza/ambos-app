8b1564e53f7c28ec67c862ed51d8176bb3323b7c
jordan <jordaniza@proton.me>
2023-10-10 17:20:35 +0400

    sw check

--- src/lib/components/pwa/ReloadPrompt.svelte
+++ src/lib/components/pwa/ReloadPrompt.svelte
@@ -10,23 +10,30 @@
 	// @ts-ignore
 	let reloadSW = __RELOAD_SW__;
 
+	const delayedCheckRefresh = 60 * 1000; // 1 minute
 	const reloadDurationInterval = 60 * 1000 * 1000; // 1 hour
 
 	const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
 		onRegistered(r) {
-			console.log('SW Registered', r);
+			console.log('[SW::Registered]', r);
 			if (reloadSW) {
+				console.log('[SW::RELOADING TIME]', Date.now() + reloadDurationInterval);
+				r &&
+					setTimeout(() => {
+						console.log('[SW::UPDATING]');
+						r.update();
+					}, delayedCheckRefresh);
 				r &&
 					setInterval(() => {
-						console.log('Checking for sw update');
+						console.log('[SW::UPDATING]');
 						r.update();
 					}, reloadDurationInterval);
 			} else {
-				console.log(`SW Registered: ${r}`);
+				console.log('[SW::NO RELOAD]');
 			}
 		},
 		onRegisterError(error) {
-			console.log('SW registration error', error);
+			console.log('[SW::REGISTRATION ERROR]', error);
 		}
 	});
 
